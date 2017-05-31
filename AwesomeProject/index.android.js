fbf78780b8b3a1501e7150dd18804583b3d5dec0
edimdendidiwangga <edimdendy@gmail.com>
2017-05-31 21:19:27 +0700

    rm redux

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -4,18 +4,16 @@ import { Container } from 'native-base';
 import App from './src/App.js'
 import Header from './src/static_components/Header.js'
 import Footer from './src/static_components/Footer.js'
-import { Provider } from 'react-redux'
-import store from './src/store'
 
 export default class AwesomeProject extends Component {
   render() {
     return (
-      <Provider store={store}>
+
         <Container >
             <App />
           <Footer />
         </Container>
-      </Provider>
+    
     );
   }
 }
